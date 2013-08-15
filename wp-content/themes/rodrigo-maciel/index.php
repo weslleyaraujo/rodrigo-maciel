5086fefad1b2d9813931935e7eb189a9ddf300bf
Weslley Araujo <weslleyaraujo20@gmail.com>
2013-08-15 20:18:35 -0300

    Add new feature:  navigation menu

--- wp-content/themes/rodrigo-maciel/index.php
+++ wp-content/themes/rodrigo-maciel/index.php
@@ -4,21 +4,21 @@
  *
  *
  * @package WordPress
- * @subpackage Theme name
- * @since Theme name 1.0
+ * @subpackage Rodrigo Maciel
+ * @since Rodrigo Maciel 1.0
  */
 	
 	// header
 	get_header();
 ?>
 
-	<?php while(have_posts()) : the_post(); ?>
-		<div class="post">
-			<h2><?php the_title(); ?></h2>
-
-			<?php the_content(); ?>
+	<div class="full-wrap">
+		<div class="wrap">
+			<div class="left-sidebar"></div>
+			<div class="right-content">
+				
+			</div>
 		</div>
-		<br />
-	<?php endwhile; ?>
+	</div>
 
 <?php get_footer(); ?>
--- wp-content/themes/rodrigo-maciel/page-example.php
+++ wp-content/themes/rodrigo-maciel/page-example.php
@@ -3,4 +3,13 @@
 	/**
 	 * Template Name: Page Example
 	 */
+
 ?>
+
+	<?php while(have_posts()) : the_post(); ?>
+		<div class="post">
+			<h2><?php the_title(); ?></h2>
+			<?php the_content(); ?>
+		</div>
+		<br />
+	<?php endwhile; ?>
