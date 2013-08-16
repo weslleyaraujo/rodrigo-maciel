94c455c1fa0d56e40e978123ece7cfe27cf3a457
Weslley Araujo <weslleyaraujo20@gmail.com>
2013-08-16 00:37:24 -0300

    Add new feature: rand images about-page

--- wp-content/themes/rodrigo-maciel/functions.php
+++ wp-content/themes/rodrigo-maciel/functions.php
@@ -1,12 +1,43 @@
 <?php
 
 /**
- * Theme name functions
+ * Rodrigo Maciel functions
  * This is the functions file from theme-name
  *
  * @package  	Wordpress
  * @author		autor (http://autor.com)
  *
  */	
+	
+	// add thumbnail suport
+	add_theme_support('post-thumbnails');
 
+	// add multiple thumbs for page
+	if (class_exists('MultiPostThumbnails')) {
+
+		new MultiPostThumbnails(array(
+			'label'     => 'Page Image 2',
+			'id'        => 'page-image-2',
+			'post_type' => 'page'
+		));
+
+		new MultiPostThumbnails(array(
+			'label'     => 'Page Image 3',
+			'id'        => 'page-image-3',
+			'post_type' => 'page'
+		));
+
+		new MultiPostThumbnails(array(
+			'label'     => 'Page Image 4',
+			'id'        => 'page-image-4',
+			'post_type' => 'page'
+		));
+
+		new MultiPostThumbnails(array(
+			'label'     => 'Page Image 5',
+			'id'        => 'page-image-5',
+			'post_type' => 'page'
+		));
+
+ 	}
 ?>
