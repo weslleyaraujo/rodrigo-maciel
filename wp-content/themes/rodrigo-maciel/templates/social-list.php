5d209e48b6f9ed179831181888672b9558b7a38d
Weslley Araujo <weslleyaraujo20@gmail.com>
2013-08-16 00:26:51 -0300

    Add new feature: rand images

---
+++ wp-content/themes/rodrigo-maciel/templates/social-list.php
@@ -0,0 +1,8 @@
+<!-- .social-list -->
+<ul class="social-list clear">
+	<li><a href="#" class="icons-social-facebook"></a></li>
+	<li><a href="#" class="icons-social-play"></a></li>
+	<li><a href="#" class="icons-social-be"></a></li>
+	<li><a href="#" class="icons-social-pinterest"></a></li>
+</ul>
+<!-- /.social-list -->
