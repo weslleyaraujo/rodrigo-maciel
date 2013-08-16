88da3b761dc8251089b55c001b37aa0d7617fe74
Weslley Araujo <weslleyaraujo20@gmail.com>
2013-08-16 00:45:42 -0300

    Add new feature: home-images rand

--- wp-content/themes/rodrigo-maciel/index.php
+++ wp-content/themes/rodrigo-maciel/index.php
@@ -18,7 +18,23 @@
 			</div>
 			<div class="right-content">
 				<div class="main-image">
-					<img src="<?php echo bloginfo('template_url') ?>/images/main-example-670x580.jpg" />
+					<?php
+						$rand = rand(1, 5);
+						switch ($rand) {
+							case 1:
+								$src = wp_get_attachment_image_src(get_post_thumbnail_id(2), 'single-post-thumbnail');
+								$src = $src[0];
+							break;
+							
+							default:
+								$src = MultiPostThumbnails::get_post_thumbnail_url('page', 'page-image-'.$rand, 2);
+							break;
+						}
+					?>
+
+					<img src="<?php echo $src; ?>" />
+
+					<!-- <img src="<?php echo bloginfo('template_url') ?>/images/main-example-670x580.jpg" /> -->
 				</div>
 				<div class="thanks-visit">
 					<p>Thank you for visit.</p>
--- wp-content/themes/rodrigo-maciel/page-about.php
+++ wp-content/themes/rodrigo-maciel/page-about.php
@@ -29,7 +29,6 @@
 					<?php while(have_posts()) : the_post(); ?>
 						<h2><?php the_title(); ?></h2>
 						<?php the_content(); ?>
-						
 					<?php endwhile; ?>
 				</div>
 				<div class="main-image">
