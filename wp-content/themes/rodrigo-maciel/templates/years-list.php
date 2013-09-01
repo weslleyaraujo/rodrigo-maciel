14787ccabc38cdae57f731dc778454efa6a4d48b
Weslley Araujo <weslleyaraujo20@gmail.com>
2013-09-01 16:46:12 -0300

    Hotfix: years list

--- wp-content/themes/rodrigo-maciel/templates/years-list.php
+++ wp-content/themes/rodrigo-maciel/templates/years-list.php
@@ -2,38 +2,42 @@
 <?php if ($years): ?>
 
 	<ul class="clear">
+		
 		<?php foreach ($years as $key => $value):
-
-			// has category
+			
+			$subPosts = $artworks->getPostsByCat($value->term_id);
 			$hasCat = has_category($value->term_id, $recent_post_id);
+			
+			if ($subPosts->have_posts()):
 		?>
-
 			<li class="year <?php echo $hasCat === true ? 'active' : ''; ?>">
+		
+		<?php 		if ($hasCat): ?>
+						<?php echo $value->name; ?>
+		<?php 		else: ?>
+						<a href="javascript:void(0);"><?php echo $value->name; ?></a>
+		<?php 		endif;
+		?>
+			<ul class="clear">
 				
-				<?php if ($hasCat): ?>
-					<?php echo $value->name; ?>
-				<?php else: ?>
-					<a href="javascript:void(0);"><?php echo $value->name; ?></a>
-				<?php endif;
-
-					$subPosts = $artworks->getPostsByCat($value->term_id);
-					if ($subPosts->have_posts()):
-				?>
-					<ul class="clear">
-				<?php
-						while($subPosts->have_posts()) : $subPosts->the_post();
-				?>
-						<li>
-							<a class="<?php echo $postTitle === get_the_title() ? 'active' : ''; ?>" href="<?php echo $postTitle === the_permalink() ? 'javascript:void(0);' : ''; ?>"><?php the_title(); ?></a>
-						</li>
-				<?php
-						endwhile;
-				?>
-					</ul>
-				<?php endif ?>
-
+		<?php		
+					while($subPosts->have_posts()) : $subPosts->the_post();
+		?>
+					<li>
+						<a class="<?php echo $postTitle === get_the_title() ? 'active' : ''; ?>" href="<?php echo $postTitle === the_permalink() ? 'javascript:void(0);' : ''; ?>"><?php the_title(); ?></a>
+					</li>
+		<?php
+					endwhile;
+		?>
+				</ul>
 			</li>
+		<?php
+			endif;
+		?>
+				
 		<?php endforeach ?>
+	
 	</ul>
+
 <?php endif ?>
 <!-- years -->
